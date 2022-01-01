14cd6f5881523570b2e521d1648d5e01b3ba2c83
Rmlyy <hello@rmly.dev>
2022-01-01 19:18:12 +0200

    change color scheme

--- config.def.h
+++ config.def.h
@@ -12,8 +12,8 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#ffffff", "#171717" },
-	[SchemeSel] = { "#ffffff", "#0066db" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeSel] = { "#000000", "#e6ebed" },
+	[SchemeOut] = { "#000000", "#000000" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 10;
